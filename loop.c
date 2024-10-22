d364a2adb1560ca71bb591c67f7617151340ee97
Jatin-Sharma29 <sharma.jatin06129@gmail.com>
2024-10-22 12:52:38 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=10;i++)
+    {
+        printf("MS Dhoni\n");
+    }
+    return 0;
+}
+// we can also write i=i+1,if we write i+1 i will not get updated and it will end up in infinite loop
