de912e8b8fc792690ad3742b6bf5e0e2cac0987b
Jatin-Sharma29 <sharma.jatin06129@gmail.com>
2024-11-12 12:26:42 +0530

    Update sumoftwomatrix.c

--- sumoftwomatrix.c
+++ sumoftwomatrix.c
@@ -15,11 +15,13 @@ int main(){
         for(int j=0;j<m;j++)
         scanf("%d",&arr1[i][j]);
     }
+    printf("Sum of two matrix=\n");
       for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             sum=arr[i][j]+arr1[i][j];
-             printf("sum=%d\n",sum);
+            printf("%d ",sum);
         }
-       
+        printf("\n");  
+    }            
     }
-}
+// for storing sum of matrix in another matrix we do it like this: result[i][j]=arr[i][j]+arr1[i][j].
