20d9d9709e00a5ee837d3d299992d059bfa6c6c2
Jatin-Sharma29 <sharma.jatin06129@gmail.com>
2024-11-12 12:12:41 +0530

    Update diagnolsum2darr.c

--- diagnolsum2darr.c
+++ diagnolsum2darr.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n,m,sum=0;
+    int n,m,sum=0,sum1=0;
     printf("Enter the size of rows and columns=");
     scanf("%d%d",&n,&m);
     int arr[n][m];
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++)
         scanf("%d",&arr[i][j]);
-    }
-    for(int i=0;i<n;i++){
+    } for(int i=0;i<n;i++){
         sum+=arr[i][i];
     }
-    printf("sum of right diagnol is =%d",sum);
+    for(int i=0,j=m-1;i<n,j>=0;i++,j--){
+            sum1+=arr[i][j];
+        }
+    printf("sum of right(main) diagnol is =%d\n",sum);
+    printf("sum of left(minor) diagnol is=%d",sum1);
 }
