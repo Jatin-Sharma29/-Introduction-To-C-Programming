e41bc3a6a956ba909272a792b31fecbbe1da0aa2
Jatin Sharma <sharma.jatin06129@gmail.com>
2024-11-27 23:11:26 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
-int main(){
-    int n,rem,sum=0,temp;
-    printf("Enter number:");        
-    scanf("%d",&n);
-    temp=n;
-    while(n!=0) {
+#include<math.h>
+int main() {
+    int n, rem, sum = 0, temp1, count = 0, temp2;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    temp1 = n;
+    // Count the number of digits
+    while (temp1 != 0) {
+        temp1 = temp1 / 10;
+        count++;
+    }
+
+    temp2 = n;
+    // Check if the number is Armstrong
+    while (n != 0) {
         rem = n % 10;
-        sum = sum + (rem * rem * rem);
+        sum += (int)pow(rem, count); 
         n = n / 10;
     }
-    if(sum == temp)
-        printf("%d is an Armstrong number.\n", temp);
+
+    if (sum == temp2)
+        printf("%d is an Armstrong number.\n", temp2);
     else
-        printf("%d is not an Armstrong number.\n", temp);
+        printf("%d is not an Armstrong number.\n", temp2);
+
+    return 0;
 }
